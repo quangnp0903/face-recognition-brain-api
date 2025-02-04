0413d9521ba08d3e375514a8e0d7b8eec351b79c
Quang Pham <quangnp0903@gmail.com>
2025-02-04 09:30:42 +0700

    fixing database connection

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ import { handleImage, handleApiCall } from './controllers/image.js';
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
